b9bf0e86d9320d2b9646793eb2d7834f3d0eb2d0
PaulBussmann <paul@medwyn.de>
2018-07-31 17:59:32 +0200

    Add AllTests building

--- BuildAndRun.cmd
+++ BuildAndRun.cmd
@@ -5,6 +5,12 @@ if not exist lib\Windows\VS2017\x86\Release mkdir lib\Windows\VS2017\x86\Release
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" CppUTest_VS2017.sln /Build "Debug|x86" /project CppUTest_VS2017 /ProjectConfig "Debug|Win32"
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" CppUTest_VS2017.sln /Build "Release|x86" /project CppUTest_VS2017 /ProjectConfig "Release|Win32"
 
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" CppUTest_VS2017.sln /Build "Debug|x86" /project ApplicationLib_VS2017 /ProjectConfig "Debug|Win32"
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" CppUTest_VS2017.sln /Build "Release|x86" /project ApplicationLib_VS2017 /ProjectConfig "Release|Win32"
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" CppUTest_VS2017.sln /Build "Debug|x86" /project AllTests_VS2017 /ProjectConfig "Debug|Win32"
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" CppUTest_VS2017.sln /Build "Release|x86" /project AllTests_VS2017 /ProjectConfig "Release|Win32"
+
 examples\AllTests\Debug\AllTests.exe -v -ojunit
 
 RunTup.cmd
