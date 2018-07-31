0131be000640a958a0aa7db18866e6d814bcd662
PaulBussmann <paul@medwyn.de>
2018-07-31 11:51:10 +0200

    Change absolute paths to relative paths in BuildAndRun.cmd Explicitely stating build configurations in BuildAndRun.cmd to fix Lib.OutputFile property

--- BuildAndRun.cmd
+++ BuildAndRun.cmd
@@ -1,11 +1,11 @@
 pushd %~dp0
 
-if not exist C:\CppUTest\lib\Windows\VS2017\x86\Debug mkdir C:\CppUTest\lib\Windows\VS2017\x86\Debug
-if not exist C:\CppUTest\lib\Windows\VS2017\x86\Release mkdir C:\CppUTest\lib\Windows\VS2017\x86\Release
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" /Build Debug CppUTest_VS2017.sln
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" /Build Release CppUTest_VS2017.sln
+if not exist lib\Windows\VS2017\x86\Debug mkdir lib\Windows\VS2017\x86\Debug
+if not exist lib\Windows\VS2017\x86\Release mkdir lib\Windows\VS2017\x86\Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" CppUTest_VS2017.sln /Build "Debug|x86" /project CppUTest_VS2017 /ProjectConfig "Debug|Win32"
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" CppUTest_VS2017.sln /Build "Release|x86" /project CppUTest_VS2017 /ProjectConfig "Release|Win32"
 
-C:\CppUTest\examples\AllTests\Debug\AllTests.exe -v -ojunit
+examples\AllTests\Debug\AllTests.exe -v -ojunit
 
 RunTup.cmd
 
